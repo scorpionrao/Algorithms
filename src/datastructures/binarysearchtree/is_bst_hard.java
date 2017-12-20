6b359b852fed0f7f8b7d22c93f62f7f7abd1de7e
Ranganatha Gonugunta <rgonugunta@clearslide.com>
2017-12-20 05:19:10 -0800

    DataStructures

---
+++ src/datastructures/binarysearchtree/is_bst_hard.java
@@ -0,0 +1,92 @@
+package datastructures.binarysearchtree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class is_bst_hard {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public class IsBST {
+        class Node {
+            int key;
+            int leftIndex;
+            int rightIndex;
+
+            Node(int key, int leftIndex, int rightIndex) {
+                this.leftIndex = leftIndex;
+                this.rightIndex = rightIndex;
+                this.key = key;
+            }
+        }
+
+        int nodes;
+        Node[] tree;
+
+        void read() throws IOException {
+            FastScanner in = new FastScanner();
+            nodes = in.nextInt();
+            tree = new Node[nodes];
+            for (int i = 0; i < nodes; i++) {
+                tree[i] = new Node(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+        }
+
+        boolean isBinarySearchTree() {
+            if(nodes <= 1) {
+                return true;
+            }
+            return isBSTUtil(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        boolean isBSTUtil(int index, int max, int min) {
+
+            if(index == -1) {
+                return true;
+            }
+
+            if(tree[index].key < min || tree[index].key > max) {
+                return false;
+            }
+            return isBSTUtil(tree[index].leftIndex, tree[index].key - 1, min)
+                    && isBSTUtil(tree[index].rightIndex, max, tree[index].key);
+        }
+    }
+
+    static public void main(String[] args) throws IOException {
+        new Thread(null, new Runnable() {
+            public void run() {
+                try {
+                    new is_bst_hard().run();
+                } catch (IOException e) {
+                }
+            }
+        }, "1", 1 << 26).start();
+    }
+    public void run() throws IOException {
+        IsBST tree = new IsBST();
+        tree.read();
+        if (tree.isBinarySearchTree()) {
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
+        }
+    }
+}
